1b5439d338762fbf6cd572fb9d9fbdb3bd1197eb
v0sem <progpabsanch@gmail.com>
2024-04-23 17:26:52 +0200

    Things

--- custom.el
+++ custom.el
@@ -3,12 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(codeium/metadata/api_key "1851b5e8-c3fe-4729-a11f-2811b6340a17")
- '(custom-safe-themes
-   '("88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "32f22d075269daabc5e661299ca9a08716aa8cda7e85310b9625c434041916af" default))
- '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
- '(package-selected-packages
-   '(synosaurus obsidian vterm python-black tree-sitter-langs tree-sitter)))
+ '(package-selected-packages '(obsidian python-black catppuccin-theme @)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
